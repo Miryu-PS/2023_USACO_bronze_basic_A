c7c8bba7483b6844a309742c1bf62a21f5241beb
Miryu-PS <53257486+Miryu-PS@users.noreply.github.com>
2023-07-22 12:41:28 +0900

    Create hanoi.cpp

---
+++ 11주차/hanoi.cpp
@@ -0,0 +1,17 @@
+#include<cstdio>
+int move(){}
+void hanoi(int N, int from, int to)
+// 1~N을 from 기둥에서 to 기둥으로 옮기는 과정을 출력
+{
+	if(N==0) return;
+	//1+2+3 == from+to+other
+	int other = 6 - from - to;
+	hanoi(N-1, from, other);
+	printf("%d %d\n", from, to);
+	hanoi(N-1, other, to);
+}
+int main()
+{
+	int N; scanf("%d",&N);
+	hanoi(N, 1, 3);
+}
